14600c0f10dc2c251f2957ddf2a735214c252e2e
Adasv9423 <76847225+Adasv9423@users.noreply.github.com>
2023-05-08 10:05:08 +0530

    Update main.dart

--- findflames/lib/main.dart
+++ findflames/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 		debugShowCheckedModeBanner: false,
 		scrollBehavior: MyCustomScrollBehavior(),
 		theme: ThemeData(
-		primarySwatch: Colors.blue,
+		primarySwatch: Colors.black,
 		),
 		home: Scaffold(
 		body: SingleChildScrollView(
